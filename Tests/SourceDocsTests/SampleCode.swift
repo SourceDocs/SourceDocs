21c7816f3303ae8db582d64533d1cb79e5e8c528
Eneko Alonso <eneko.alonso@gmail.com>
2017-10-04 17:06:33 -0700

    Initial code port for standalone tool

---
+++ Tests/SourceDocsTests/SampleCode.swift
@@ -0,0 +1,46 @@
+//
+//  SampleCode.swift
+//  sourcedocs
+//
+//  Created by Eneko Alonso on 10/4/17.
+//
+
+import Foundation
+
+/// Describes an animal hierarchy of things
+public protocol Animal {
+    /// Print a greeting message
+    func speak()
+}
+
+/// Describes a thing that can be named
+public protocol Nameable {
+    /// Name of the thing being named
+    var name: String { get }
+}
+
+extension Nameable: CustomStringConvertible {
+    /// Describes a named thing
+    public var description: String {
+        return "Hello, my name is \(name)."
+    }
+}
+
+/// Describes an entity that can "speak"
+public protocol Speaker {
+    /// Print a greeting message
+    func speak()
+}
+
+/// Common animal many people have as pet
+public struct Dog: Animal, Nameable {
+    /// All dogs should have a name
+    public let name: String
+}
+
+extension Dog: Speaker {
+    /// Dogs know how to speak!
+    public func speak() {
+        print("Woof, woof!")
+    }
+}
