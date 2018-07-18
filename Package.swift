b3637c606f053dd3736122fbd2075233c252e7d1
Jim Hildensperger <jim.hildensperger@ing.nl>
2018-07-18 13:15:12 +0200

    Update dependencies

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
         .executable(name: "sourcedocs", targets: ["SourceDocs"]),
     ],
     dependencies: [
-        .package(url: "../SourceKitten", .branch("fix/comments")),
+        .package(url: "https://github.com/jhildensperger/SourceKitten.git", .branch("fix/comments")),
         .package(url: "https://github.com/Carthage/Commandant.git", from: "0.13.0"),
         .package(url: "https://github.com/eneko/MarkdownGenerator.git", from: "0.4.0"),
         .package(url: "https://github.com/onevcat/Rainbow", from: "3.0.0"),
