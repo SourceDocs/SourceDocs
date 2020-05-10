4bf5ef1ad9a60d7e55fbd79b6f776e7065e05af6
Eneko Alonso <eneko.alonso@gmail.com>
2020-05-10 14:40:44 -0700

    Add unit test for issue #43

---
+++ Tests/SourceDocsLibTests/FilenameTests.swift
@@ -0,0 +1,25 @@
+//
+//  FilenameTests.swift
+//  
+//
+//  Created by Eneko Alonso on 5/10/20.
+//
+
+import XCTest
+@testable import SourceDocsLib
+
+class FilenameTests: XCTestCase {
+
+    func testMethodFilename() throws {
+        let dict: SwiftDocDictionary = [
+            "key.accessibility": "source.lang.swift.accessibility.public",
+            "key.kind": "source.lang.swift.decl.function.free",
+            "key.name": "method(paramA:paramB:paramC:)"
+        ]
+        let options = MarkdownOptions(collapsibleBlocks: false, tableOfContents: false, minimumAccessLevel: .public)
+        let method = try XCTUnwrap(MarkdownMethod(dictionary: dict, options: options))
+        let files = MarkdownIndex().makeFiles(with: [method], basePath: ".")
+        XCTAssertEqual(files.first?.filePath, "./method(paramA_paramB_paramC_).md")
+    }
+
+}
