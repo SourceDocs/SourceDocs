2528e0fd06b8e92a7d33304372f44a7f5b45f377
Eneko Alonso <eneko.alonso@gmail.com>
2020-06-03 19:27:04 -0700

    Prep for 1.2.0

--- Tests/SourceDocsCLITests/VersionCommandTests.swift
+++ Tests/SourceDocsCLITests/VersionCommandTests.swift
@@ -12,7 +12,7 @@ class VersionCommandTests: XCTestCase {
 
     func testVersion() throws {
         let result = try system(command: binaryURL.path, parameters: ["version"], captureOutput: true)
-        XCTAssertEqual(result.standardOutput, "SourceDocs v1.1.0")
+        XCTAssertEqual(result.standardOutput, "SourceDocs v1.2.0")
     }
 
 }
